c8a7dcd896f236e3f2e5f639272ecd312ae166df
Jestin-FE <jestinfe@naver.com>
2026-04-28 20:55:55 +0900

    사용자 테이블에 최종로그인일시, 계정잠금여부, 비밀번호오류횟수 등 3개의 컬럼 추가. 사용자 테이블 DDL 위에 주석 추가.

--- src/main/resources/db/mcee_auctn.sql
+++ src/main/resources/db/mcee_auctn.sql
@@ -1,4 +1,8 @@
 /* mcee_auctn의 schema를 정의한다. */
+
+/*
+ * 사용자
+ */
 CREATE TABLE tb_user(
       id VARCHAR2(20) -- 아이디
     , pswd VARCHAR2(25) -- 비밀번호
@@ -6,6 +10,9 @@ CREATE TABLE tb_user(
     , mbl_telno VARCHAR2(11) -- 휴대전화번호
     , rsdc_addr VARCHAR2(200) -- 거주지주소
     , BRDT DATE -- 생년월일
+    , LAST_LGN_DT DATE -- 최종로그인일시
+    , acnt_lck_yn VARCHAR2(1) -- 계정잠금여부
+    , pswd_err_nmtm NUMBER(2) -- 비밀번호오류횟수
     , REG_ID VARCHAR2(20) -- 등록자
     , REG_DT DATE -- 등록일시
     , MOD_ID VARCHAR2(20) -- 수정자
